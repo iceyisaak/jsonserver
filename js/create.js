56aa46fafe163801f35720953ce61df3fec3ebe6
Icey Isaak <me@iceyisaak.com>
2020-12-20 11:22:28 +0100

    JSON Server Test

---
+++ js/create.js
@@ -0,0 +1,25 @@
+// javascript for create.html
+const form = document.querySelector('form');
+
+const createPost = async (e) => {
+
+ e.preventDefault();
+ const doc = {
+  title: form.title.value,
+  body: form.body.value,
+  likes: 0
+ };
+
+ await fetch('http://localhost:3000/posts', {
+  headers: {
+   "Content-Type": "application/json"
+  },
+  method: 'POST',
+  body: JSON.stringify(doc)
+ });
+
+ window.location.replace('/index.html');
+
+};
+
+form.addEventListener('submit', createPost);
